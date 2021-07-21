96152c3d5c19db75cea416558ec8e4bdfa6dbc28
tanishquaradhika <85229614+tanishquaradhika@users.noreply.github.com>
2021-07-21 19:40:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school,player;
+function preload()
+{
+
+}
+
+function setup() {
+  createCanvas(1366,2700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+    player = new Player(width/2,height-25);
+    translate(0,-player.spt.y+height-150);
+ }
+
+function draw() {
+  background("skyblue");
+  keyPressed();
+
+
+
+ for(var i=0;i<6;i++){
+var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+bottomGrass1.shapeColor="green";
+if(i%2===0){
+var road = createSprite(683,height-150-(i*400)-grassHeight,width,300,)
+road.shapeColor="black";
+}
+ }
+ for(var i = 0 ;i<40;i++){
+   cars=new Car(2);
+   carGroup1.add(cars.spt);
+ }
+ 
+
+   for(var i = 0 ;i<100;i++){
+     logs=new Log(2);
+     logGroup1.add(logs.spt);
+   }
+
+
+
+ for(var i=1;i<logGroup1.length;i++){
+   if(logGroup1[i].x<0){
+     logGroup1[i].x=width;
+   }
+ }
+  for(var i=1;i<carGroup1.length;i<1366,i>0,i++){
+   if(carGroup1[i].x<0){
+     carGroup1[i].x=width;
+   }
+ }
+
+
+  drawSprites();
+}
+function keyPressed(){
+  if(keyCode==UP_ARROW){
+    player.move(0,-2);
+  }
+  else if(keyCode==DOWN_ARROW){
+    player.move(0,2);
+  }
+  else if(keyCode==RIGHT_ARROW){
+    player.move(2,0);
+  }
+  else if(keyCode==LEFT_ARROW){
+    player.move(-2,0);
+  }
+}
